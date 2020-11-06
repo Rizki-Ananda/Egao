865ddf9c2b5399ab2342c747a4421346b1c9e4a4
Rizki Ananda <rananda198@gmail.com>
2020-11-06 17:07:44 +0700

    process.env.PORT || 5000

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var storage = multer.diskStorage({
 var upload = multer({ storage: storage });
 
 const app = express();
-const PORT = 18320;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static("public"));
 
